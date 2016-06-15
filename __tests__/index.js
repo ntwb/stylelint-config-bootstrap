35ade47b1b819571181b250846897832ad7d7bf4
Stephen Edgar <stephen@netweb.com.au>
2016-06-15 10:27:18 +1000

    feat: Add initial `stylelint-config-bootstrap` config

---
+++ __tests__/index.js
@@ -0,0 +1,42 @@
+import config from "../"
+import stylelint from "stylelint"
+import test from "ava"
+
+const validCss = (`
+a {
+  top: .2em;
+}
+`)
+
+const invalidCss = (`
+a {
+  top: 0.2em;
+}
+`)
+
+test("no warnings with valid css", t => {
+  return stylelint.lint({
+    code: validCss,
+    config: config,
+  })
+  .then(data => {
+    const { errored, results } = data
+    const { warnings } = results[0]
+    t.falsy(errored, "no errored")
+    t.is(warnings.length, 0, "flags no warnings")
+  })
+})
+
+test("a warning with invalid css", t => {
+  return stylelint.lint({
+    code: invalidCss,
+    config: config,
+  })
+  .then(data => {
+    const { errored, results } = data
+    const { warnings } = results[0]
+    t.truthy(errored, "errored")
+    t.is(warnings.length, 1, "flags one warning")
+    t.is(warnings[0].text, "Unexpected leading zero (number-leading-zero)", "correct warning text")
+  })
+})
---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "plugins": [
+    "stylelint-scss",
+  ],
+  "rules": {
+    "number-leading-zero": "never",
+  },
+}
